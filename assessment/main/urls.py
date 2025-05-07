2ed3ab223d10c3a458f55dbaf60b53c3eb0c0ac7
phalsus <odafeomajuwa@gmail.com>
2025-05-07 10:51:22 +0100

    adding uuid

--- assessment/main/urls.py
+++ assessment/main/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('customerRegistration/', include('customerRegistration.urls')),
+    path('', include('customerRegistration.urls')),
 ]
